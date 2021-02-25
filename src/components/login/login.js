f2663d8d5c9ccbf1a92245c5a3131bd8c8cd0d34
SantanaPereira <santana.pereira@sapo.pt>
2021-02-25 15:42:35 +0000

    atualizados o index.js root

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,32 +1,26 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import axios from "axios";
 import swal from "sweetalert";
-import { Link } from "react-router-dom";
 import Recaptcha from "react-recaptcha";
-
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import * as loginActions from "../../actions/login.action";
+import { server } from "../../constants";
 const LoginSchema = Yup.object().shape({
   username: Yup.string()
     .min(2, "username is Too Short!")
     .max(50, "username is Too Long!")
     .required("Username is Required"),
-    recaptcha: Yup.string().required(),
-    password: Yup.string().required("Password is required")
+  recaptcha: Yup.string().required(),
+  password: Yup.string().required("Password is required"),
 });
 
+const Login = (props) => {
+  const dispatch = useDispatch();
+  const loginReducer = useSelector(({ loginReducer }) => loginReducer);
 
-
-class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      alert: null
-    };
-  }
-
-  initilizeRecaptcha = async => {
+  const initilizeRecaptcha = async () => {
     const script = document.createElement("script");
     script.src = "https://www.google.com/recaptcha/api.js";
     script.async = true;
@@ -34,57 +28,31 @@ class Login extends Component {
     document.body.appendChild(script);
   };
 
-  
-  componentDidMount() {
+  useEffect(() => {
+    initilizeRecaptcha();
+    if (localStorage.getItem(server.TOKEN_KEY) != null) {
+      return props.history.push("/dashboard");
+    }
+    let notify = props.match.params["notify"];
+    if (notify !== undefined) {
+      if (notify === "error") {
+        swal("Activation Fail please try again !", "", "error");
+      } else if (notify === "success") {
+        swal("Activation Success your can login !", "", "success");
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    this.initilizeRecaptcha();
 
-    if (localStorage.getItem("TOKEN_KEY") != null) {
-        return this.props.history.goBack();
-     }
-
-     let notify = this.props.match.params["notify"]
-     if(notify !== undefined){
-       if(notify === 'error'){
-         swal("Activation Fail please try again !", '', "error")
-       }else if(notify === 'success'){
-         swal("Activation Success your can login !", '', "success").then(value => {
-          window.location.replace("/login");
-        });
-       }
-      
-     }
-   }
-  
-
-  submitForm = (values, history) => {
-    axios
-    .post(process.env.REACT_APP_API_URL + "login", values)
-      .then(res => {
-        if (res.data.result === "success") {
-          localStorage.setItem("TOKEN_KEY", res.data.token);
-          swal("Success!", res.data.message, "success").then(value => {
-            history.push("/dashboard");
-          });
-        } else if (res.data.result === "error") {
-          swal("Error!", res.data.message, "error");
-        }
-      })
-      .catch(error => {
-        console.log(error);
-        return swal("Error!", error.message, "error");
-      });
-  };
-
-  showForm = ({
+  const showForm = ({
     values,
     errors,
     touched,
     handleChange,
     handleSubmit,
-    onSubmit,
+    setFieldValue,
     isSubmitting,
-    setFieldValue
   }) => {
     return (
       <form onSubmit={handleSubmit}>
@@ -96,17 +64,17 @@ class Login extends Component {
             value={values.username}
             className="form-control"
             placeholder="Username"
-            autoComplete="Nome do User"
-             // eslint-disable-next-line react/jsx-no-duplicate-props
+           
+            // eslint-disable-next-line react/jsx-no-duplicate-props
             className={
               errors.username && touched.username
                 ? "form-control is-invalid"
                 : "form-control"
             }
           />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-user"></span>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
             </div>
           </div>
           {errors.username && touched.username ? (
@@ -123,52 +91,51 @@ class Login extends Component {
             value={values.password}
             className="form-control"
             placeholder="Password"
-            autoComplete="Password min 8"
-             // eslint-disable-next-line react/jsx-no-duplicate-props
+            // eslint-disable-next-line react/jsx-no-duplicate-props
             className={
               errors.password && touched.password
                 ? "form-control is-invalid"
                 : "form-control"
             }
           />
-          <div className="input-group-append">
-            <div className="input-group-text">
-              <span className="fas fa-lock"></span>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
             </div>
           </div>
           {errors.password && touched.password ? (
-            <small id="passwordHelp" className="text-danger">
+            <small id="passwordHelp" class="text-danger">
               {errors.password}
             </small>
           ) : null}
         </div>
-                  <div className="form-group">
-                    <label>Recaptcha Validation</label>
-                    <Recaptcha
-                      sitekey={process.env.REACT_APP_RECAPTCHA_KEY}
-                      render="explicit"
-                      theme="light"
-                      verifyCallback={(response) => {
-                        setFieldValue("recaptcha", response);
-                      }}
-                      onloadCallback={() => {
-                        console.log("done loading!");
-                      }}
-                    />
-                    {errors.recaptcha && touched.recaptcha && <p>{errors.recaptcha}</p>}
-                  </div>
-        <div className="row">
-          <div className="col-8">
-            <div className="icheck-primary">
+        <div className="form-group">
+          <label>Recaptcha Validation</label>
+          <Recaptcha
+            sitekey={process.env.REACT_APP_RECAPCHA_KEY}
+            render="explicit"
+            theme="light"
+            verifyCallback={(response) => {
+              setFieldValue("recaptcha", response);
+            }}
+            onloadCallback={() => {
+              console.log("done loading!");
+            }}
+          />
+          {errors.recaptcha && touched.recaptcha && <p>{errors.recaptcha}</p>}
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
               <input type="checkbox" id="remember" />
-              <label htmlFor="remember">Remember Me</label>
+              <label for="remember">Remember Me</label>
             </div>
           </div>
-          <div className="col-4">
+          <div class="col-4">
             <button
               type="submit"
               disabled={isSubmitting}
-              className="btn btn-primary btn-block"
+              class="btn btn-primary btn-block"
             >
               Sign In
             </button>
@@ -178,48 +145,46 @@ class Login extends Component {
     );
   };
 
-  render() {
-
-    return (
-      <div className="login-page">
-        <div className="register-box">
-          <div className="register-logo">
-            <a href="../../../public/AdminLTE/index2.html">
-              <b>Basic</b>POS
-            </a>
-          </div>
-          <div className="card">
-            <div className="card-body register-card-body">
-              <p className="login-box-msg">Sign in to start your session</p>
-
-              <Formik
-                initialValues={{
-                  username: "",
-                  password: ""
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                  this.submitForm(values, this.props.history);
-                  setSubmitting(false);
-                }}
-                validationSchema={LoginSchema}
-              >
-                {/* {this.showForm()}            */}
-                {props => this.showForm(props)}
-              </Formik>
-              <p className="mb-1">
-                <Link to="/password/forgot">I forgot my password</Link>
-              </p>
-              <p className="mb-0">
-                <Link to="/register">Register a new membership</Link>
-              </p>
-            </div>
-            {/* /.form-box */}
-          </div>
-          {/* /.card */}
+  return (
+    <div class="login-page">
+      <div className="register-box">
+        <div className="register-logo">
+          <a href="../../index2.html">
+            <b>Basic</b>POS
+          </a>
         </div>
+        <div className="card">
+          <div className="card-body register-card-body">
+            <p className="login-box-msg">Sign in to start your session</p>
+
+            <Formik
+              initialValues={{
+                username: "",
+                password: "",
+                recaptcha: "",
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                dispatch(loginActions.login(values, props.history));
+                setSubmitting(false);
+              }}
+              validationSchema={LoginSchema}
+            >
+              {/* {this.showForm()}            */}
+              {(props) => showForm(props)}
+            </Formik>
+            <p class="mb-1">
+              <Link to="/password/forgot">I forgot my password</Link>
+            </p>
+            <p class="mb-0">
+              <Link to="/register">Register a new membership</Link>
+            </p>
+          </div>
+          {/* /.form-box */}
+        </div>
+        {/* /.card */}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import logger from "redux-logger";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render( <App />, document.getElementById('root'));
+var middlewares = null;
+
+if (process.env.REACT_APP_IS_PRODUCTION === "1") {
+  middlewares = applyMiddleware(thunk);
+} else {
+  middlewares = applyMiddleware(thunk, logger);
+}
+
+const store = createStore(reducers, composeWithDevTools(middlewares));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
